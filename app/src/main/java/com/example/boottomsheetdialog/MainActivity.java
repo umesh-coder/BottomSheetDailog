0df2c3194c2d3a96f76f38962877d6e515452c97
Levi Ackerman <umeshshelare@gmail.com>
2024-05-10 15:46:32 +0530

    added swipe down gesture

--- app/src/main/java/com/example/boottomsheetdialog/MainActivity.java
+++ app/src/main/java/com/example/boottomsheetdialog/MainActivity.java
@@ -3,8 +3,7 @@ package com.example.boottomsheetdialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,6 +12,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.boottomsheetdialog.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,16 +21,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
@@ -38,16 +34,32 @@ public class MainActivity extends AppCompatActivity {
 
         Button OpenBottomSheet = findViewById(R.id.open_bottom_sheet);
 
-        OpenBottomSheet.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        BottomSheetDialog bottomSheet =
-                                new BottomSheetDialog();
-                        bottomSheet.show(getSupportFragmentManager(),
-                                "ModalBottomSheet");
-                    }
-                });
+
+        View open_view = findViewById(R.id.container);
+
+        open_view.setOnTouchListener(new OnSwipeTouchListener(this) {
+
+
+            public void onSwipeDown() {
+
+                BottomSheetDialog bottomSheet = new BottomSheetDialog();
+                bottomSheet.show(getSupportFragmentManager(), "ModalBottomSheet");
+
+                Toast.makeText(getApplicationContext(), "Swipe Down", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        OpenBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheet = new BottomSheetDialog();
+                bottomSheet.show(getSupportFragmentManager(), "ModalBottomSheet");
+            }
+        });
+
+
     }
+
 
 }
